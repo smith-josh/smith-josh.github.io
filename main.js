fc892cf509e31819c52e2cd777a027c66f9d2b13
smith-josh <joshnathansmith@byui.edu>
2020-09-19 09:34:11 -0600

    big update

---
+++ main.js
@@ -0,0 +1,4 @@
+var year = document.getElementById("year");
+year.innerText = new Date().getFullYear();
+var update = document.getElementById("last-update");
+update.innerText = document.lastModified;
